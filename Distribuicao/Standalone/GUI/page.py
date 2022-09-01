2ee80776e5587b5c9ba86524fd90a87fdd592bd1
Lucas Amaral <lucasamaral.souzaz@gmail.com>
2022-09-01 17:51:16 -0300

    Dimensionamentos das telas

--- Distribuicao/Standalone/GUI/page.py
+++ Distribuicao/Standalone/GUI/page.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'pageYZeewj.ui'
+## Form generated from reading UI file 'pageHNYwkz.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.3.1
 ##
@@ -15,23 +15,33 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QSizePolicy, QStackedWidget,
-    QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QFrame, QLabel, QSizePolicy,
+    QStackedWidget, QVBoxLayout, QWidget)
 
 class Ui_application_pages(object):
     def setupUi(self, application_pages):
         if not application_pages.objectName():
             application_pages.setObjectName(u"application_pages")
-        application_pages.resize(989, 556)
+        application_pages.resize(1227, 714)
         self.page_1 = QWidget()
         self.page_1.setObjectName(u"page_1")
         self.verticalLayout = QVBoxLayout(self.page_1)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.label_2 = QLabel(self.page_1)
+        self.frame = QFrame(self.page_1)
+        self.frame.setObjectName(u"frame")
+        self.frame.setStyleSheet(u"background-color: rgb(40, 44, 52);")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.label_2 = QLabel(self.frame)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setAlignment(Qt.AlignCenter)
+        self.label_2.setGeometry(QRect(4, 1, 61, 21))
+        self.label_2.setStyleSheet(u"color: rgb(255, 255, 255);")
+        self.label_6 = QLabel(self.frame)
+        self.label_6.setObjectName(u"label_6")
+        self.label_6.setGeometry(QRect(1151, 667, 61, 31))
+        self.label_6.setStyleSheet(u"color: rgb(255, 255, 255);")
 
-        self.verticalLayout.addWidget(self.label_2)
+        self.verticalLayout.addWidget(self.frame)
 
         application_pages.addWidget(self.page_1)
         self.page_2 = QWidget()
@@ -86,7 +96,8 @@ class Ui_application_pages(object):
 
     def retranslateUi(self, application_pages):
         application_pages.setWindowTitle(QCoreApplication.translate("application_pages", u"StackedWidget", None))
-        self.label_2.setText(QCoreApplication.translate("application_pages", u"Pagina Principal", None))
+        self.label_2.setText(QCoreApplication.translate("application_pages", u"TextLabel", None))
+        self.label_6.setText(QCoreApplication.translate("application_pages", u"TextLabel", None))
         self.label_3.setText(QCoreApplication.translate("application_pages", u"Hora do Ponto", None))
         self.label.setText(QCoreApplication.translate("application_pages", u"Chat Box", None))
         self.label_4.setText(QCoreApplication.translate("application_pages", u"Perfil", None))
