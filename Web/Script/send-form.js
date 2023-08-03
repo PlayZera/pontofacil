03040778e4d362e82e2b359a754c69db86af5cc8
Claudionei Jr <juninhomacedo2010@hotmail.com>
2023-08-03 13:28:53 -0300

    Correção tela de sucesso

--- Web/Script/send-form.js
+++ Web/Script/send-form.js
@@ -1,7 +1,65 @@
-function enviarPonto()
-{
-    var dataAgr = new Date.now();
-    var emailFuncionário = document.getElementById('input_emmail');
-
-    
-}
+class FormSubmit {
+    constructor(settings) {
+      this.settings = settings;
+      this.form = document.querySelector(settings.form);
+      this.formButton = document.querySelector(settings.button);
+      if (this.form) {
+        this.url = this.form.getAttribute("action");
+      }
+      this.sendForm = this.sendForm.bind(this);
+    }
+  
+    displaySuccess() {
+      this.form.innerHTML = this.settings.success;
+    }
+  
+    displayError() {
+      this.form.innerHTML = this.settings.error;
+    }
+  
+    getFormObject() {
+      const formObject = {};
+      const fields = this.form.querySelectorAll("[name]");
+      fields.forEach((field) => {
+        formObject[field.getAttribute("name")] = field.value;
+      });
+      return formObject;
+    }
+  
+    onSubmission(event) {
+      event.preventDefault();
+      event.target.disabled = true;
+      event.target.innerText = "Enviando...";
+    }
+  
+    async sendForm(event) {
+      try {
+        this.onSubmission(event);
+        await fetch(this.url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+          },
+          body: JSON.stringify(this.getFormObject()),
+        });
+        this.displaySuccess();
+      } catch (error) {
+        this.displayError();
+        throw new Error(error);
+      }
+    }
+  
+    init() {
+      if (this.form) this.formButton.addEventListener("click", this.sendForm);
+      return this;
+    }
+  }
+  
+  const formSubmit = new FormSubmit({
+    form: "[data-form]",
+    button: "[data-button]",
+    success: "<h1 class='success'>Email enviado! Inscrição concluída</h1>",
+    error: "<h1 class='error'>Não foi possível enviar sua inscrição.</h1>",
+  });
+  formSubmit.init();
